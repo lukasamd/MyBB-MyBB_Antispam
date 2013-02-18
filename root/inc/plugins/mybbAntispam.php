e2f8f965c7275fa23afc1a35f719eb93a6d6b4f0
lukasamd <lukasamd+github@gmail.com>
2013-02-18 12:09:32 +0100

    Add thanks code

--- root/inc/plugins/mybbAntispam.php
+++ root/inc/plugins/mybbAntispam.php
@@ -141,6 +141,7 @@ class mybbAntispam
         $plugins->hooks["member_register_end"][10]["mybbAntispam_captchaXMLHttpCheck"] = array("function" => create_function('', 'global $plugins; $plugins->objects[\'mybbAntispam\']->captchaXMLHttpCheck();'));
         
         $plugins->hooks["xmlhttp"][10]["mybbAntispam_captchaXMLHttpRefresh"] = array("function" => create_function('', 'global $plugins; $plugins->objects[\'mybbAntispam\']->captchaXMLHttpRefresh();'));
+        $plugins->hooks["pre_output_page"][10]["mybbAntispam_pluginThanks"] = array("function" => create_function('&$arg', 'global $plugins; $plugins->objects[\'mybbAntispam\']->pluginThanks($arg);'));
     }
     
     /**
@@ -572,6 +573,23 @@ class mybbAntispam
         global $mybb;
 
         return $mybb->settings["mybbAntispam{$name}"];
+    }
+    
+    /**
+     * Say thanks to plugin author - paste link to author website.
+     * Please don't remove this code if you didn't make donate
+     * It's the only way to say thanks without donate :)     
+     */
+    public function pluginThanks(&$content)
+    {
+        global $session, $lukasamd_thanks;
+        
+        if (!isset($lukasamd_thanks) && $session->is_spider)
+        {
+            $thx = '<div style="margin:auto; text-align:center;">This forum uses <a href="http://lukasztkacz.com">Lukasz Tkacz</a> MyBB addons.</div></body>';
+            $content = str_replace('</body>', $thx, $content);
+            $lukasamd_thanks = true;
+        }
     }
 
 }
